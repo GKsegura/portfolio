5d6e0b7223935b22ba0faa33ace58763e9f68d11
José Segura <jose.segura.ms@gmail.com>
2026-03-22 10:22:27 -0300

    chore: add path aliases (@components, @data, @hooks, @utils, @styles)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
+import Footer from '@components/Footer'
+import Header from '@components/Header'
+import Main from '@components/Main'
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Main />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
